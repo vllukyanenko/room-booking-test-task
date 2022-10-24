5367175bbe543efe266166e01ccbdaf1f16e2d28
Vladyslav Lukianenko <vladyslav.lukianenko@aimprosoft.com>
2022-10-24 09:18:13 +0000

    Add service layer

---
+++ src/main/java/com/aim/booking/config/BookingTimeConfiguration.java
@@ -0,0 +1,17 @@
+package com.aim.booking.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "booking.time")
+public class BookingTimeConfiguration {
+
+  private int initialAvailableBookingWeekDayNumber;
+  private int finalAvailableBookingWeekDayNumber;
+  private int initialAvailableBookingHours;
+  private int finalAvailableBookingHour;
+
+}
